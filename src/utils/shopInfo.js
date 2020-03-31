472b897caeb9f93afb27aa68ce02f3653566ceef
Michael Nguyen <thainguyen99.dev@gmail.com>
2020-03-31 14:40:41 +0700

    add shop instagram

--- src/utils/shopInfo.js
+++ src/utils/shopInfo.js
@@ -1,7 +1,7 @@
 export const shopFacebook = 'Daisy Bakery';
 export const shopFacebookUrl = 'https://www.facebook.com/DAISY-Bakery-105345414230149/';
 export const shopInstagram = 'Daisy Bakery';
-export const shopInstagramUrl = 'https://www.instagram.com/';
+export const shopInstagramUrl = 'https://www.instagram.com/_b.daisyyy/';
 export const shopName = 'Daisy Bakery';
 export const shopYoutubeUrl = 'https://www.youtube.com/channel/UCQ5AzmHCppUGZuwag-97CDg?view_as=subscriber&fbclid=IwAR0E8G8qdRKhLjzc68RWuagF4eURptU1mHLlAdsLTrFxLlONkewGGzIOM4E';
 export const shopGmail = 'banhdaisy13@gmail.com';
